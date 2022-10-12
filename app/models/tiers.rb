89b78ad8bf2c41d8a90640f75287a27df9ca23d2
Jordan Halfman <jordanhalfman@gmail.com>
2022-10-12 13:51:23 -0700

    add has_many through: relationship between tiers and tribes

--- app/models/tiers.rb
+++ app/models/tiers.rb
@@ -1,3 +1,4 @@
 class Tier < Sinatra::Base
     has_many :cards
+    has_many :tribes, through: cards
 end
--- app/models/tribes.rb
+++ app/models/tribes.rb
@@ -1,3 +1,4 @@
 class Tribe < Sinatra::Base
     has_many :cards
+    has_many :tiers, through: cards
 end
