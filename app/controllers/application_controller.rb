f7e61c965431cd25e18e91f85041766cd119cf05
Jordan Halfman <jordanhalfman@gmail.com>
2022-10-21 11:33:50 -0700

    fixed query parameter route for fitler parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,20 +15,21 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/cards" do
-    Card.all.to_json
-  end
-  
-  get "/cards/:stars/:tribe" do
-    if params[:stars] == "0" && params[:tribe] == "0"
+    stars = params[:stars]
+    tribe = params[:tribe]
+    if stars == "0" && tribe == "0"
       Card.all.to_json
-    elsif params[:stars] == "0"
-      Card.where(tribe_id: params[:tribe]).to_json
-    elsif params[:tribe] == "0"
-      Card.where(tier_id: params[:stars]).to_json
+    elsif params == {}
+      Card.all.to_json
+    elsif stars == "0" || stars == nil
+      Card.where(tribe_id: tribe).to_json
+    elsif tribe == "0" || tribe == nil
+      Card.where(tier_id: stars).to_json
     else
-      Card.where(tier_id: params[:stars], tribe_id: params[:tribe]).to_json
+      Card.where(tier_id: stars, tribe_id: tribe).to_json
     end
   end
+  
 
   get "/builds" do
     Build.all.to_json
