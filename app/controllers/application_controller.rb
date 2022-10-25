d586f496aef844a0ce7d002fae91e51f0805a603
Jordan Halfman <jordanhalfman@gmail.com>
2022-10-24 19:54:47 -0700

    using array for post request:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,50 +1,17 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+
   get "/" do
     { message: "Hearthstone Battlegrounds Build Lister!" }.to_json
   end
 
-  get "/tribes" do
-    Tribe.all.to_json
-  end
-
-  get "/tiers" do
-    Tier.all.to_json
-  end
-
   get "/cards" do
-    stars = params[:stars]
-    tribe = params[:tribe]
-    if stars == "0" && tribe == "0"
-      Card.all.to_json
-    elsif params == {}
-      Card.all.to_json
-    elsif stars == "0" || stars == nil
-      Card.where(tribe_id: tribe).to_json
-    elsif tribe == "0" || tribe == nil
-      Card.where(tier_id: stars).to_json
-    else
-      Card.where(tier_id: stars, tribe_id: tribe).to_json
-    end
-  end
-
-  get "/cards/:id" do
-    Card.find(params[:id]).to_json
-  end
-  
+    Card.all.to_json(include: [:tier, :tribe])
+  end  
 
   get "/builds" do
-    Build.all.to_json
-  end
-
-  get "/builds/:id" do
-    build_list = BuildCard.where(build_id: params[:id])
-    card_list = build_list.map do |card|
-      Card.find(card.card_id)
-    end
-    card_list.to_json
+    Build.all.to_json(include: :cards)
   end
 
   patch "/builds/:id" do
@@ -52,49 +19,21 @@ class ApplicationController < Sinatra::Base
     updatedBuildCard.first.update(
       card_id: params[:replacementId]
     )
-    
-    updatedBuildCard.first.card.to_json
+    #updatedBuildCard.first.card.to_json
+    Build.all.to_json(include: :cards)
   end
 
   post "/builds" do
-    #Expecting {
-      #name: "",
-      #cards: [
-        #{cardName: "", cardId: null, cardImage: null} x7
-      #]
-    #}
     build = Build.create(
       name: params[:name]
     )
-    build_card = BuildCard.create(
-      card_id: params[:cards][0][:cardId],
-      build_id: build.id
-    )
-    build_card = BuildCard.create(
-      card_id: params[:cards][1][:cardId],
-      build_id: build.id
-    )
-    build_card = BuildCard.create(
-      card_id: params[:cards][2][:cardId],
-      build_id: build.id
-    )
-    build_card = BuildCard.create(
-      card_id: params[:cards][3][:cardId],
-      build_id: build.id
-    )
-    build_card = BuildCard.create(
-      card_id: params[:cards][4][:cardId],
-      build_id: build.id
-    )
-    build_card = BuildCard.create(
-      card_id: params[:cards][5][:cardId],
-      build_id: build.id
-    )
-    build_card = BuildCard.create(
-      card_id: params[:cards][6][:cardId],
-      build_id: build.id
-    )
-    build.to_json
+    params[:cards].each do |card|
+      BuildCard.create(
+        card_id: card[:cardId],
+        build_id: build.id
+      )
+    end
+    build.to_json(include: :cards)
   end
 
   delete "/builds/:id" do
@@ -103,8 +42,7 @@ class ApplicationController < Sinatra::Base
       item.destroy
     end
     build.destroy
-    build.to_json
+    Build.all.to_json(include: :cards)
   end
-
 end
 
--- app/models/cards.rb
+++ app/models/cards.rb
@@ -1,4 +1,6 @@
 class Card < ActiveRecord::Base
     belongs_to :tier
     belongs_to :tribe
+    has_many :build_cards
+    has_many :builds, through: :build_cards
 end
